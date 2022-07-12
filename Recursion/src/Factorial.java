dab8f4b10dc16013d20254ae1de031c7dbbff6c1
tarun <21mcc2036@cuchd.in>
2022-07-12 11:02:41 +0530

    commit

---
+++ Recursion/src/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(String[] args) {
+       int a= fact(6);
+        System.out.println(a);
+    }
+
+    private static int fact(int i) {
+        if(i==0) return 1;
+        return i*fact(i-1);
+
+    }
+}
